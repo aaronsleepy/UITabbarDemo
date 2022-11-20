ef46561520b4ca370b1fca0b758cccbdae78caff
aaronsleepy <aaron@kmong.com>
2022-11-20 14:40:48 +0900

    feat: CollectionView 구현

--- UITabbarDemo/SearchViewController.swift
+++ UITabbarDemo/SearchViewController.swift
@@ -29,15 +29,35 @@ class SearchViewController: UIViewController {
 
 extension SearchViewController : UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        return 24
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ResultCell", for: indexPath) as? ResultCell else {
+            return UICollectionViewCell()
+        }
+        
+        let imageName = "animal\(indexPath.item + 1)"
+        cell.configure(imageName)
+        return cell
     }
 }
 
 extension SearchViewController : UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let iterItemSpacing: CGFloat = 1
+        let width = (collectionView.bounds.width - iterItemSpacing * 2) / 3
+        let height = width
+        
+        return CGSize(width: width, height: height)
+    }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
 }
 
